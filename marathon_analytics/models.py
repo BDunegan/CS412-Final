9f101997253d1e469bbc16640f7f0cee0a5db276
BDunegan <109842239+BDunegan@users.noreply.github.com>
2024-12-08 18:46:16 -0500

    Delete marathon_analytics directory

--- marathon_analytics/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Result(models.Model):
-    last_name = models.TextField()
-    first_name = models.TextField()
--- marathon_analytics/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views 
-urlpatterns = [
-    # map the URL (empty string) to the view
-    path(r'', views.ResultsListView.as_view(), name='home'),
-]
